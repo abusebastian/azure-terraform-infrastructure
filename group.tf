f09bb12fc9b397e12faaa489ea1593b010ffb89c
Abu Sebastian <abusebastian@MBP16Abebastian.home>
2024-07-28 20:38:08 +0200

    final

--- group.tf
+++ group.tf
@@ -1,20 +1,20 @@
-# data "azuread_client_config" "current" {}
+data "azuread_client_config" "current" {}
 
-# resource "azuread_user" "aduser" {
-#   for_each = var.users
-#   display_name        = each.value.display_name
-#   user_principal_name = each.value.user_principal_name
-#   password            = each.value.password
-#   force_password_change = false
-# }
+resource "azuread_user" "aduser" {
+  for_each = var.users
+  display_name        = each.value.display_name
+  user_principal_name = each.value.user_principal_name
+  password            = each.value.password
+  force_password_change = false
+}
 
-# resource "azuread_group" "adgroup" {
-#   for_each         = var.groups
-#   display_name     = each.value.name
-#   owners           = [data.azuread_client_config.current.object_id]
-#   security_enabled = true
+resource "azuread_group" "adgroup" {
+  for_each         = var.groups
+  display_name     = each.value.name
+  owners           = [data.azuread_client_config.current.object_id]
+  security_enabled = true
 
-#   members = [
-#     for user in azuread_user.aduser : user.id
-#   ]
-# }
+  members = [
+    for user in azuread_user.aduser : user.id
+  ]
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -6,24 +6,24 @@ myContainerApp= "mycontainerappname"
 myLogAnalytics= "myloganalyticsname"
 myContainerAppEnv= "mycontainerappenv"
 
-# groups = {
-#   group1 = {
-#     name        = "Group One"
-#   }
-#   group2 = {
-#     name        = "Group Two"
-#   }
-# }
+groups = {
+  group1 = {
+    name        = "Group One"
+  }
+  group2 = {
+    name        = "Group Two"
+  }
+}
 
-# users = {
-#   user1 = {
-#     display_name        = "User 1"
-#     user_principal_name = "user1@test.com"
-#     password            = "password"
-#   }
-#   user2 = {
-#     display_name        = "User 2"
-#     user_principal_name = "user2@test.com"
-#     password            = "password"
-#   }
-# }
+users = {
+  user1 = {
+    display_name        = "User 1"
+    user_principal_name = "user1@test.com"
+    password            = "password"
+  }
+  user2 = {
+    display_name        = "User 2"
+    user_principal_name = "user2@test.com"
+    password            = "password"
+  }
+}
